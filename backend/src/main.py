832145886a1a0a53558f5ab8f1d3ec035744c1a6
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-04-07 15:53:58 +0100

    backend FastApi - add CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.src.router import router as currency_routes
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(currency_routes)
